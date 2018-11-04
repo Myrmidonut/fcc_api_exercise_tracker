52344d4578cff843d4273a1252408d43086e5d8a
Glitch (fcc-api-exercise-tracker-fred) <none>
2018-11-04 19:26:26 +0000

    code style rework

--- server.js
+++ server.js
@@ -58,97 +58,91 @@ const userSchema = new mongoose.Schema({
 // model
 const User = mongoose.model("User", userSchema);
 
-// DONE
 // POST username 
 // to /api/exercise/new-user
 // return object with username and _id
-app.post("/api/exercise/new-user", function(req, res) {
-  let username = req.body.username;
+app.post("/api/exercise/new-user", (req, res) => {
+  const username = req.body.username;
   
-  User.create({username: username}, function(err, data) {
+  User.create({username: username}, (err, data) => {
   //   User.create({username: username, exercise: [{description: null, duration: null, date: null}] }, function(err, data) {
     if (err) console.log(err);
     else {
-      User.findOne({username: username}).select("username _id").exec(function(err, data) {
+      User.findOne({username: username}).select("username _id").exec((err, data) => {
         if (err) console.log(err);
-        else 
-          res.json(data);
+        else res.json(data);
       });
     }
   });
 });
 
-// DONE
 // POST exercise with: userId(_id), description, duration, and optionally date
 // empty date is replaced with current date
 // to /api/exercise/add
 // return object with all user data and only the added exercise data
 app.post("/api/exercise/add", function(req, res) {
-  let userId = req.body.userId;
-  let description = req.body.description;
-  let duration = req.body.duration;
+  const userId = req.body.userId;
+  const description = req.body.description;
+  const duration = req.body.duration;
   let date = "";
   
   if (req.body.date) date = req.body.date;
   else {
-    let currentDate = new Date();
-    let month = currentDate.getMonth() + 1;
+    const currentDate = new Date();
+    const month = currentDate.getMonth() + 1;
     date = currentDate.getFullYear() + "-" + month + "-" + currentDate.getDate();
   }
   
-  let newData = {description: description, duration: duration, date: date};
+  const newData = {
+    description: description,
+    duration: duration, date: date
+  };
   
-  User.findByIdAndUpdate(userId, {$push: {exercise: newData}}, {new: true}, function(err, data) {
+  User.findByIdAndUpdate(userId, {$push: {exercise: newData}}, {new: true}, (err, data) => {
     if (err) console.log(err);
-    else res.json({"_id": data.id, "username": data.username, "exercise": newData});
+    else res.json({
+      "_id": data.id,
+      "username": data.username,
+      "exercise": newData
+    });
   });
 });
 
-// DONE
-// GET
-// to /api/exercise/users
+// GET /api/exercise/users
 // return array of all users with objects with username and _id
-app.get("/api/exercise/users", function(req, res) {
-  User.find().select("username _id").exec(function(err, data) {
+app.get("/api/exercise/users", (req, res) => {
+  User.find().select("username _id").exec((err, data) => {
     if (err) console.log(err);
     else res.json(data);
   })
 })
 
-// DONE
-// GET
-// to /api/exercise/log
+// GET /api/exercise/log
 // /api/exercise/log?{userId}[&from][&to][&limit]
 // return object with all data and exercise counter
 // return exercises from ... to ... and limit to ... (Date format yyyy-mm-dd, limit = int)
 // "/api/exercise/log?:id(&:from)?(&:to)?(&:limit)?"
 // https://fcc-exercise-tracker-fred.glitch.me/api/exercise/log?userId=5b40f6de02d0e61805ff146c&from=2014-1-1&to=2018-12-12&limit=100
-app.get("/api/exercise/log", function(req, res) {
-  let id = req.query.userId;
-  let from = req.query.from;
-  let to = req.query.to;
-  let limit = req.query.limit;
+app.get("/api/exercise/log", (req, res) => {
+  const id = req.query.userId;
+  const from = req.query.from;
+  const to = req.query.to;
+  const limit = req.query.limit;
   let exercisesFromTo = undefined;
   let exercisesFromToLimit = undefined;
 
-  User.findById(id, function(err, data) {
+  User.findById(id, (err, data) => {
     if (err) console.log(err);
     else {
-      let exerciseCount = data.exercise.length;
+      const exerciseCount = data.exercise.length;
       exercisesFromTo = data.exercise;
       
       if (from && to) {
-        exercisesFromTo = data.exercise.filter(function(e) {
-          return (e.date >= from && e.date <= to);
-        });
+        exercisesFromTo = data.exercise.filter(e => e.date >= from && e.date <= to);
       } else if (from && !to) {
-        exercisesFromTo = data.exercise.filter(function(e) {
-          return e.date >= from;
-        });
+        exercisesFromTo = data.exercise.filter(e => e.date >= from);
       } else if (!from && to) {
-        exercisesFromTo = data.exercise.filter(function(e) {
-          return e.date <= to;
-        });
+        exercisesFromTo = data.exercise.filter(e => e.date <= to);
       }
       
       if (limit) {
@@ -157,7 +151,12 @@ app.get("/api/exercise/log", function(req, res) {
         exercisesFromToLimit = exercisesFromTo;
       }
 
-      res.json({"_id": data.id, "username": data.username, "exercises": exercisesFromToLimit, "Total Exercises": exerciseCount});
+      res.json({
+        "_id": data.id,
+        "username": data.username,
+        "exercises": exercisesFromToLimit,
+        "Total Exercises": exerciseCount
+      });
     }
   });
 });
